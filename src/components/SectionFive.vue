61b1f3e42684558b734af0236df72a45eaaabd01
huynhanh24h <anhdev24h@gmail.com>
2024-08-22 13:29:33 +0700

    complete

--- src/components/SectionFive.vue
+++ src/components/SectionFive.vue
@@ -1,3 +1,28 @@
+<script setup>
+import {ref} from 'vue'
+const images = ref([
+  '',
+  '../assets/gallery/img-2.png',
+  '../assets/gallery/img-3.png',
+  '../assets/gallery/img-4.png',
+  '../assets/gallery/img-5.png',
+  '../assets/gallery/img-6.png',
+  '../assets/gallery/img-7.png',
+  '../assets/gallery/img-8.png',
+  '../assets/gallery/img-9.png',
+  '../assets/gallery/img-10.png',
+  '../assets/gallery/img-11.png',
+  '../assets/gallery/img-12.png',
+  '../assets/gallery/img-13.png',
+  '../assets/gallery/img-14.png',
+  '../assets/gallery/img-15.png',
+  '../assets/gallery/img-16.png',
+  '../assets/gallery/img-17.png',
+  '../assets/gallery/img-18.png',
+  '../assets/gallery/img-19.png',
+  '../assets/gallery/img-20.png',
+])
+</script>
 <template>
   <section class="session__five py-5">
     <p class="five__title text-center text-uppercase">
@@ -20,7 +45,187 @@
                         <div class="row d-flex justify-content-center align-content-center">
                             <div class="col-md-9">
                                 <div class="img__slide__item">
-                                    <img src="../assets/five__left.png" class="card-img-top" alt="..." />
+                                    <img src="../assets/five__right.png" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row d-flex justify-content-center align-content-center">
+                            <div class="col-md-9">
+                                <div class="img__slide__item">
+                                    <img src="../assets/gallery/img-1.png" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row d-flex justify-content-center align-content-center">
+                            <div class="col-md-9">
+                                <div class="img__slide__item">
+                                    <img src="../assets/gallery/img-2.png" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row d-flex justify-content-center align-content-center">
+                            <div class="col-md-9">
+                                <div class="img__slide__item">
+                                    <img src="../assets/gallery/img-3.png" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row d-flex justify-content-center align-content-center">
+                            <div class="col-md-9">
+                                <div class="img__slide__item">
+                                    <img src="../assets/gallery/img-4.png" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row d-flex justify-content-center align-content-center">
+                            <div class="col-md-9">
+                                <div class="img__slide__item">
+                                    <img src="../assets/gallery/img-5.png" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row d-flex justify-content-center align-content-center">
+                            <div class="col-md-9">
+                                <div class="img__slide__item">
+                                    <img src="../assets/gallery/img-6.png" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row d-flex justify-content-center align-content-center">
+                            <div class="col-md-9">
+                                <div class="img__slide__item">
+                                    <img src="../assets/gallery/img-7.png" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row d-flex justify-content-center align-content-center">
+                            <div class="col-md-9">
+                                <div class="img__slide__item">
+                                    <img src="../assets/gallery/img-8.png" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row d-flex justify-content-center align-content-center">
+                            <div class="col-md-9">
+                                <div class="img__slide__item">
+                                    <img src="../assets/gallery/img-9.png" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row d-flex justify-content-center align-content-center">
+                            <div class="col-md-9">
+                                <div class="img__slide__item">
+                                    <img src="../assets/gallery/img-10.png" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row d-flex justify-content-center align-content-center">
+                            <div class="col-md-9">
+                                <div class="img__slide__item">
+                                    <img src="../assets/gallery/img-11.png" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row d-flex justify-content-center align-content-center">
+                            <div class="col-md-9">
+                                <div class="img__slide__item">
+                                    <img src="../assets/gallery/img-12.png" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row d-flex justify-content-center align-content-center">
+                            <div class="col-md-9">
+                                <div class="img__slide__item">
+                                    <img src="../assets/gallery/img-13.png" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row d-flex justify-content-center align-content-center">
+                            <div class="col-md-9">
+                                <div class="img__slide__item">
+                                    <img src="../assets/gallery/img-14.png" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row d-flex justify-content-center align-content-center">
+                            <div class="col-md-9">
+                                <div class="img__slide__item">
+                                    <img src="../assets/gallery/img-15.png" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row d-flex justify-content-center align-content-center">
+                            <div class="col-md-9">
+                                <div class="img__slide__item">
+                                    <img src="../assets/gallery/img-16.png" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row d-flex justify-content-center align-content-center">
+                            <div class="col-md-9">
+                                <div class="img__slide__item">
+                                    <img src="../assets/gallery/img-17.png" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row d-flex justify-content-center align-content-center">
+                            <div class="col-md-9">
+                                <div class="img__slide__item">
+                                    <img src="../assets/gallery/img-18.png" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row d-flex justify-content-center align-content-center">
+                            <div class="col-md-9">
+                                <div class="img__slide__item">
+                                    <img src="../assets/gallery/img-19.png" class="card-img-top" alt="..." />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="row d-flex justify-content-center align-content-center">
+                            <div class="col-md-9">
+                                <div class="img__slide__item">
+                                    <img src="../assets/gallery/img-20.png" class="card-img-top" alt="..." />
                                 </div>
                             </div>
                         </div>
@@ -42,9 +247,6 @@
         <h3 class="text-center text-uppercase banner__text">Câu chuyện của chúng mình</h3>
   </section>
 </template>
-<script setup>
-import { RouterLink } from "vue-router";
-</script>
 <style>
 .five__title {
   margin: 0;
@@ -71,7 +273,6 @@ import { RouterLink } from "vue-router";
 
 .img__slide__item{
   width: 100%;
-  height: 800px;
 }
 
 .img__slide__item > img{
@@ -91,7 +292,6 @@ import { RouterLink } from "vue-router";
     }
     .img__slide__item{
       width: 100%;
-      height: 500px;
     }
 }
 </style>
